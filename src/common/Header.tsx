d229720f669fce11654fffbdead0f7007783ffe9
Subhadeep-CS <dassubhadeep631@gmail.com>
2025-01-18 23:53:37 +0530

    intoduce my thingd

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -2,11 +2,30 @@ import { AlignJustify, CircleUserRound } from "lucide-react";
 import Logo from "../assets/images/youtube_logo.png";
 import { toggleSideBarMenu } from "../redux/slices/globalSlice";
 import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_SUGGESTION_API } from "../utils/constant";
 const Header: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const dispatch = useDispatch();
   const toggleMenu = () => {
     dispatch(toggleSideBarMenu());
   };
+
+  const getSearchSuggestions = async (): Promise<void> => {
+    try {
+      const response: Response = await fetch(
+        YOUTUBE_SEARCH_SUGGESTION_API + searchQuery
+      );
+      const data: string[] = await response.json();
+      console.log("Results", data[1]);
+    } catch (error) {
+      console.error("Error fetching suggestions:", error);
+    }
+  };
+
+  useEffect(() => {
+    getSearchSuggestions();
+  }, [searchQuery]);
   return (
     <div className="grid grid-flow-col p-2 shadow-lg items-center">
       <div className="col-span-1 flex justify-center items-center">
@@ -18,6 +37,8 @@ const Header: React.FC = () => {
           type="text"
           placeholder="Search"
           className="w-1/2 p-2 rounded-l-full outline-gray-300 border border-gray-400"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button className="rounded-r-full py-2 px-4 bg-gray-100 border border-gray-400">
           🔍
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,3 @@
 export const YOUTUBE_VIDEO_URL:string=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${import.meta.env.VITE_APP_API_KEY}`;
+
+export const YOUTUBE_SEARCH_SUGGESTION_API:string=`http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
